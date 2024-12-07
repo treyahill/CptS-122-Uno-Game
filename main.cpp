2210ac2a2f5435a4c6e0b8c2d1cbb0f0cd7ff304
ItsRuusty <itsruusty@gmail.com>
2024-12-06 20:09:10 -0800

    1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Game.hpp"
 #include <SFML/Graphics.hpp>
+#include <filesystem>
 
 int main() {
 
@@ -13,27 +14,27 @@ int main() {
 
         sf::Texture red_card;
        
-        if (!red_card.loadFromFile("UnoCards/Red/R.Zero.png")) {
+        if (!red_card.loadFromFile("UnoCards/R.Zero.png")) {
             std::cout << "error loading image" << std::endl;
+            return -1;
         }
 
         sf::Sprite Red_card; Red_card.setTexture(red_card);
+        Red_card.setScale(56.f / red_card.getSize().x, 89.f / red_card.getSize().y);
+        Red_card.setPosition((winW - 56) / 2, (winH - 89) / 2);
 
-        while (window.isOpen())
-        {
+        while (window.isOpen()) {
             sf::Event event;
-            while (window.pollEvent(event))
-            {
+            while (window.pollEvent(event)) {
                 if (event.type == sf::Event::Closed)
                     window.close();
             }
 
-         
-
             // Clear the window
             window.clear();
 
-            // Draw objects here
+            // Draw the sprite
+            window.draw(Red_card);
 
             // Display the changes
             window.display();
